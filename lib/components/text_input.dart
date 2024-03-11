a6b3f7bca64658781cc43538157f52141e9f7926
LilCisaille <damien.richer@epitech.eu>
2024-03-11 15:28:12 +0100

    #12 update of the text input

--- lib/components/text_input.dart
+++ lib/components/text_input.dart
@@ -1,21 +1,43 @@
 import 'package:flutter/material.dart';
 
 class CustomTextField extends StatelessWidget {
+  final String name;
+
+  const CustomTextField({
+    Key? key,
+    required this.name,
+  }) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.symmetric(horizontal: 24.0),
-      width: MediaQuery.of(context).size.width - 32.0,
-      child: TextField(
-        decoration: InputDecoration(
-          contentPadding: const EdgeInsets.all(16.0),
-          filled: true,
-          fillColor: const Color(0xffffe4a1),
-          border: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(16),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(bottom: 8.0, left: 24.0),
+          child: Text(
+            '$name :',
+            style: TextStyle(
+              fontSize: 18.0,
+              fontWeight: FontWeight.bold,
+            ),
           ),
         ),
-      ),
+        Container(
+          margin: const EdgeInsets.symmetric(horizontal: 24.0),
+          width: MediaQuery.of(context).size.width - 32.0,
+          child: TextField(
+            decoration: InputDecoration(
+              contentPadding: const EdgeInsets.all(16.0),
+              filled: true,
+              fillColor: const Color(0xffffe4a1),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(16),
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
