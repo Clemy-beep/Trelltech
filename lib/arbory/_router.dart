b95c9ae8ec8004277c1dee554d695047364d9d5d
Clemy-beep <clementine.digny@gmail.com>
2024-03-11 17:21:21 +0100

    #3 #2 organizations & boards see page started

--- lib/arbory/_router.dart
+++ lib/arbory/_router.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:trelltech/views/orgs_and_boards_lists_screen.dart';
 
 import 'debug_screen.dart';
 import 'details_screen.dart';
-import 'home_screen.dart';
+import '../views/home_screen.dart';
 
 /// The route configuration.
 final GoRouter router = GoRouter(
@@ -25,6 +26,12 @@ final GoRouter router = GoRouter(
           builder: (BuildContext context, GoRouterState state) {
             return const DetailsScreen();
           },
+        ),
+        GoRoute(
+            path: 'home',
+            builder: (BuildContext context, GoRouterState state) {
+              return const OrgsAndBoardsListScreens();
+              },
         ),
       ],
     ),
---
+++ lib/views/orgs_and_boards_lists_screen.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:trelltech/arbory/auth_service.dart';
+import 'package:trelltech/arbory/user_info_service.dart';
+import 'package:trelltech/components/custom_bottom_navigation_bar.dart';
+import 'package:trelltech/components/checkbox.dart';
+import 'package:trelltech/components/custom_card.dart';
+import 'package:trelltech/components/custom_list_tile.dart';
+import 'package:trelltech/components/custom_expansion_tile.dart';
+import 'package:trelltech/components/delete_text_button.dart';
+
+class OrgsAndBoardsListScreens extends StatelessWidget{
+  // Constructs a [OrgsAndBoardsListScreens]
+  const OrgsAndBoardsListScreens({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Consumer<TokenMember>(
+          builder: (context, tokenMember, child) {
+            return Container(
+              alignment: Alignment.centerRight,
+              child: Text(
+                'welcome ${tokenMember.member?.fullName ?? 'no user'} !',
+                textAlign: TextAlign.end,
+                textWidthBasis: TextWidthBasis.longestLine,
+                style: const TextStyle(
+                  fontFamily: 'LexendExa',
+                  fontSize: 14,
+                  color: Colors.black,
+                ),
+              )
+            );
+          },
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Text('Orgs and Boards List Screens'),
+             CustomCard(title: 'workspace', subtitle: 'subtitle', date: 'date',),
+              CustomListTile(cardTitle: 'Hey', memberAvatar: ''),
+              CustomExpansionTile(title: 'workspaces', subtitle: 'fo', children: [
+                CustomListTile(cardTitle: '"qff', memberAvatar: '')
+              ]),
+              CustomIconButton(onPressed: () => {print('delete')}),
+              CustomIconButton(onPressed: () => {print('edit')}),
+            ],
+          ),
+        ),
+      ),
+      bottomNavigationBar: const CustomNavigationBar(),
+    );
+    throw UnimplementedError();
+  }
+}
